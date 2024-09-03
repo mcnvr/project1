034799c20e6a11ca97cdfe5d8d878c64f2bc35e6
mcnvr <ml29cannavaro@gmail.com>
2024-09-03 13:23:26 -0400

    Creating MVC

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -1,6 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddControllers();
 var app = builder.Build();
-
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
 app.UseDefaultFiles();
+app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=App}/{action=Index}/{id?}");
 app.Run();
